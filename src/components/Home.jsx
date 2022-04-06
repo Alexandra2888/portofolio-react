32b20f30f0f6a4ac6aee82ab906ef872850485d1
Alexandra2888 <moldovan.alexandra28@gmail.com>
2022-04-06 21:34:01 +0300

    fix: fixing btn view work

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
 
 import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
+import { Link } from 'react-scroll';
+
 
 const Home = () => {
     return (
@@ -15,17 +17,16 @@ const Home = () => {
                     I'm a Frontend Developer.
                 </h2>
                 <p className='text-[#8892b0] py-4 max-w-[700px]'>
-                    I’m a frontend developer specializing in building (and occasionally
-                    designing) exceptional digital experiences. Currently, I’m focused on
+                    I’m a frontend developer specializing in building digital experiences. Currently, I’m focused on
                     building responsive web applications.
                 </p>
                 <div>
-                    <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+                    <Link to='work' className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
                         View Work
                         <span className='group-hover:rotate-90 duration-300'>
               <HiArrowNarrowRight className='ml-3 ' />
             </span>
-                    </button>
+                    </Link>
                 </div>
             </div>
         </div>
