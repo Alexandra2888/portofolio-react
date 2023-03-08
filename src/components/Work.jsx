ff251136a6c8f69283a35128265122906a525d2e
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-03-08 16:07:16 +0200

    Adding new project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -23,6 +23,7 @@ import gymantic from "../assets/gYmantic.png";
 import scarlet from "../assets/scarlet.png";
 import scramble from "../assets/scramble.png";
 import film from "../assets/film.png";
+import backroads from "../assets/backroads.png";
 
 
 
@@ -648,7 +649,31 @@ const Work = () => {
                         </div>
                     </div>
 
+ <div
+                        style={{ backgroundImage: `url(${backroads})` }}
+                        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+                    >
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                React 18 & Vite
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href='https://backroads28.netlify.app/'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                        Demo
+                                    </button>
+                                    </a>
+                                <a href='https://github.com/Alexandra2888/Backroads'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                        Code
+                                    </button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
 
+                    
 
                 </div>
 
