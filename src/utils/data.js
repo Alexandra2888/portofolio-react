653272834566a3b5b334b75ae0bdea5f8f2db953
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-03-31 11:07:51 +0300

    refactor Work into a smaller component-WorkItem

---
+++ src/utils/data.js
@@ -0,0 +1,218 @@
+import learn from '../assets/learn-resource.png';
+import practice from '../assets/css-practice.png';
+import meetup from '../assets/meetup-app.png';
+import monster from '../assets/monster.png';
+import music from '../assets/music-app.png';
+import play from '../assets/play.png';
+import management from '../assets/project-management.png';
+import quiz from '../assets/quiz-app.png';
+import robofriends from '../assets/robofriends.png';
+import recipe from '../assets/recipe.png';
+import weather from '../assets/weather.png';
+import forum from '../assets/forum.png';
+import brain from '../assets/brain.png';
+import map from '../assets/map.png';
+import projects from '../assets/projects.png';
+import trilo from '../assets/trilo.png';
+import zeal from "../assets/zeal.png";
+import natours from "../assets/natours.png";
+import nexter from "../assets/nexter.jpg";
+import phoxul from "../assets/phoxul.png";
+import gymantic from "../assets/gYmantic.png";
+import scarlet from "../assets/scarlet.png";
+import scramble from "../assets/scramble.png";
+import film from "../assets/film.png";
+import backroads from "../assets/backroads.png";
+
+
+export const work = [
+    {
+        id: 1,
+        image: learn,
+        technology: 'React JS Application',
+        demo: 'https://resource-learn-app.netlify.app/',
+        code:'https://github.com/Alexandra2888/learn_resources'
+    },
+    {
+        id: 2,
+        image: music,
+        technology: 'HTML, CSS, JS',
+        demo: 'https://js-music-app-js.netlify.app/',
+        code:'https://github.com/Alexandra2888/Music-Player'
+    },
+    {
+        id: 3,
+        image: practice,
+        technology: 'HTML, CSS',
+        demo: 'https://promotional-landing-page.netlify.app/',
+        code:'https://github.com/Alexandra2888/uHost-CSS-Practice'
+    },
+    {
+        id: 4, 
+        image: play,
+        technology: 'Vue JS Application',
+        demo: 'https://practical-curie-5c667b.netlify.app/',
+        code:'https://github.com/Alexandra2888/CSS3-Perspective-playground-Vuejs-'
+    },
+    {
+        id: 5,
+        image: meetup,
+        technology: 'React JS',
+        demo: 'https://meetup-application.netlify.app/',
+        code:'https://github.com/Alexandra2888/Meetup-app'
+    },
+    {
+        id: 6,
+        image: management,
+        technology: 'HTML, CSS, JS',
+        demo: 'https://project-management-appjs.netlify.app/login.html',
+        code:'https://github.com/Alexandra2888/Project_management_app-Bittnet'
+    },
+    {
+        id: 7,
+        image: monster,
+        technology: 'Vue JS',
+        demo: 'https://vuejs-monster-game.netlify.app/',
+        code:'https://github.com/Alexandra2888/Monster-Slayer-game-Vue.js'
+    },
+    {
+        id: 8,
+        image: quiz,
+        technology: ' Vue JS',
+        demo: 'https://quiz-game-vuejs.netlify.app/',
+        code:'https://github.com/Alexandra2888/Quiz-App'
+    },
+    {
+        id: 9,
+        image: robofriends,
+        technology:'  React JS',
+        demo: 'https://robofriends-reactjs.netlify.app/',
+        code:'https://github.com/Alexandra2888/Robofriends'
+    },
+    {
+        id: 10,
+        image: recipe,
+        technology: 'HTML, CSS',
+        demo: 'https://elegant-bohr-4c5d75.netlify.app/',
+        code:'https://github.com/Alexandra2888/Recipes-CSS-practising'
+    },
+    {
+        id: 11,
+        image: weather,
+        technology: 'Vue JS',
+        demo: 'https://app-weather-vuejs.netlify.app/',
+        code:'https://github.com/Alexandra2888/weather-vue.js'
+    },
+    {
+        id: 12,
+        image: robofriends,
+        technology: 'React JS-Hooks',
+        demo: 'https://robofriends-hooks-reactjs.netlify.app/',
+        code:'https://github.com/Alexandra2888/Robofriends-hooks'
+    },
+    {
+        id: 13,
+        image: brain,
+        technology: 'React JS',
+        demo: '',
+        code:'https://github.com/Alexandra2888/Smart-Brain'
+    },
+    {
+        id: 14,
+        image: brain,
+        technology: 'React JS',
+        demo: '',
+        code:'https://github.com/Alexandra2888/Smart-Brain-API'
+    },
+    {
+        id: 15,
+        image: forum,
+        technology: 'Vue JS',
+        demo: 'https://forum-vuejs.netlify.app/',
+        code:'https://github.com/Alexandra2888/forum-vue.js'
+    },
+    {
+        id: 16,
+        image: map,
+        technology: 'Typescript',
+        demo: '',
+        code:'https://github.com/Alexandra2888/Find-Place-Google-API-Ts-'
+    },
+    {
+        id: 17,
+        image: projects,
+        technology: 'Typescript',
+        demo: '',
+        code:'https://github.com/Alexandra2888/Drag-Drop-Project-TS-'
+    },
+    {
+        id: 18,
+        image: trilo,
+        technology: 'HTML, CSS',
+        demo: 'https://trillo28.netlify.app/',
+        code:'https://github.com/Alexandra2888/Trillo'
+    },
+    {
+        id: 19,
+        image: zeal,
+        technology: 'HTML, CSS, JS',
+        demo: 'https://zeal28.netlify.app/',
+        code:'https://github.com/Alexandra2888/Zeal'
+    },
+    {
+        id: 20,
+        image: natours,
+        technology: 'HTML, CSS',
+        demo: 'https://natours28.netlify.app/',
+        code:'https://github.com/Alexandra2888/Natours'
+    },
+    {
+        id: 21,
+        image: phoxul,
+        technology: 'HTML, CSS, JS',
+        demo: 'https://phoxul-studio28.netlify.app/',
+        code:'https://github.com/Alexandra2888/Phoxul_Studio'
+    },
+    {
+        id: 22,
+        image: nexter,
+        technology: 'HTML, CSS',
+        demo: 'https://nexter28.netlify.app/',
+        code:'https://github.com/Alexandra2888/Nexter'
+    },
+    {
+        id: 23,
+        image: gymantic,
+        technology: 'HTML, CSS, JS',
+        demo: 'https://gymantic28.netlify.app/',
+        code:'https://github.com/Alexandra2888/gYmantic'
+    },
+    {
+        id: 24,
+        image: scarlet,
+        technology: 'HTML, CSS',
+        demo: 'https://portofolio-website28.netlify.app/#',
+        code:'https://github.com/Alexandra2888/Scarlett-portofolio'
+    },
+    {
+        id: 25,
+        image: scramble,
+        technology: 'Angular 14',
+        demo: 'https://scramble-word.netlify.app/',
+        code:'https://github.com/Alexandra2888/Scramble'
+    },
+    {
+        id: 26,
+        image: film,
+        technology: 'Angular 14',
+        demo: '',
+        code:'https://github.com/Alexandra2888/film-app'
+    },
+    {
+        id: 27,
+        image: backroads,
+        technology: 'React JS',
+        demo: 'https://backroads28.netlify.app/',
+        code:'https://github.com/Alexandra2888/Backroads'
+    }
+]
